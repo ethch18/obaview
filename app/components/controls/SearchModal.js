ec4f92d7c053a23531a0274c1c27c8a61fc46db3
Ethan Chau <echau18@cs.uw.edu>
2018-06-30 20:33:43 -0700

    more friendly error messages

--- app/components/controls/SearchModal.js
+++ app/components/controls/SearchModal.js
@@ -3,14 +3,18 @@ import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import PropTypes from 'prop-types';
 import Spinner from 'react-spinner';
 import axios from 'axios';
-import { ENDPOINTS } from '../../util/Constants';
+import {
+    ENDPOINTS,
+    GENERAL_ERROR,
+    INVALID_ROUTE_ERROR
+} from '../../util/Constants';
 import SearchDirection from './SearchDirection';
 
 const propTypes = {
     input: PropTypes.string.isRequired,
     updater: PropTypes.func.isRequired,
     closer: PropTypes.func.isRequired,
-    stopCache: PropTypes.object.isRequired,
+    stopCache: PropTypes.object.isRequired
 };
 
 export default class SearchModal extends React.Component {
@@ -18,27 +22,35 @@ export default class SearchModal extends React.Component {
         super(props);
         // TODO: fancy logic for converting route no. to route id
         this.state = {
-            routeId: this.props.input,
+            routeId: this.props.input
         };
     }
 
     componentDidMount() {
-        axios.get(`${ENDPOINTS['BASE_URL']}${ENDPOINTS['STOPS_FOR_ROUTE']}${this.state.routeId}`)
-            .then((response) => {
+        axios
+            .get(
+                `${ENDPOINTS['BASE_URL']}${ENDPOINTS['STOPS_FOR_ROUTE']}${
+                    this.state.routeId
+                }`
+            )
+            .then(response => {
                 // console.log(response);
                 if (response.data.code != 200 || !response.data.data) {
-                    this.setState({ error: response.data.text });
-                }
-                else {
-                    this.setState({ 
-                        data: response.data.data.entry.stopGroupings[0].stopGroups, 
-                        refData: response.data.data.references.stops,
+                    // this.setState({ error: response.data.text });
+                    this.setState({ error: INVALID_ROUTE_ERROR });
+                } else {
+                    this.setState({
+                        data:
+                            response.data.data.entry.stopGroupings[0]
+                                .stopGroups,
+                        refData: response.data.data.references.stops
                     });
                 }
             })
-            .catch((error) => {
+            .catch(error => {
                 // console.log(error);
-                this.setState({ error});
+                // this.setState({ error });
+                this.setState({ error: GENERAL_ERROR });
             });
     }
 
@@ -47,12 +59,13 @@ export default class SearchModal extends React.Component {
         if (this.state.error) {
             content = (
                 <div>
-                    <div>An error occurred</div>
-                    <div>{this.state.error}</div>
+                    <div> An error occurred!</div>
+                    <div className="searchmodal-errorcontent">
+                        {this.state.error}
+                    </div>
                 </div>
             );
-        }
-        else if (this.state.data && this.state.refData) {
+        } else if (this.state.data && this.state.refData) {
             const directions = [];
             for (let i = 0; i < this.state.data.length; i++) {
                 const heading = this.state.data[i].name.name;
@@ -61,32 +74,25 @@ export default class SearchModal extends React.Component {
                     <SearchDirection
                         heading={heading}
                         stops={stops}
-                        stopCache={this.props.stopCache} 
-                        refData={this.state.refData} 
+                        stopCache={this.props.stopCache}
+                        refData={this.state.refData}
                         updater={this.props.updater}
                         key={`${heading}-${i}`}
                     />
                 );
             }
-            content = (
-                <div>
-                    {directions}
-                </div>
-            );
-        }
-        else {
+            content = <div>{directions}</div>;
+        } else {
             content = <Spinner />;
         }
 
         return (
             <div>
-                <Modal isOpen className={"show"} toggle={this.props.closer}>
-                        <ModalHeader toggle={this.props.closer}>
-                            {`Search Results for \"${this.props.input}\"`}
-                        </ModalHeader>
-                        <ModalBody>
-                            {content}
-                        </ModalBody>
+                <Modal isOpen className={'show'} toggle={this.props.closer}>
+                    <ModalHeader toggle={this.props.closer}>
+                        {`Search Results for \"${this.props.input}\"`}
+                    </ModalHeader>
+                    <ModalBody>{content}</ModalBody>
                 </Modal>
             </div>
         );
--- app/util/Constants.js
+++ app/util/Constants.js
@@ -1,11 +1,16 @@
 export const ENDPOINTS = {
-    BASE_URL: "https://obaview.azurewebsites.net",
-    STOPS_FOR_ROUTE: "/stops-for-route/",
-    STOP: "/stop/",
-    ARRIVALS_DEPARTURES: "/arrivals-departures/",
+    BASE_URL: 'https://obaview.azurewebsites.net',
+    STOPS_FOR_ROUTE: '/stops-for-route/',
+    STOP: '/stop/',
+    ARRIVALS_DEPARTURES: '/arrivals-departures/'
 };
 
 export const COOKIE = 'obaview-stopIds';
 
 export const REFRESH_SECONDS = 60;
 export const REFRESH_MILLI = 1000 * REFRESH_SECONDS;
+
+export const INVALID_ROUTE_ERROR =
+    "The route ID you've entered was not recognized - did you enter a route number by accident?";
+export const GENERAL_ERROR =
+    'Please check your internet connection and try again.';
