283e7a8b4e948faff28418f7864f48f294a85875
Ethan Chau <echau18@cs.uw.edu>
2022-04-29 21:06:27 -0700

    update server host

--- app/util/Constants.js
+++ app/util/Constants.js
@@ -3,7 +3,7 @@ const isDev = window.location.hostname === 'localhost';
 export const ENDPOINTS = {
     BASE_URL: isDev
         ? 'http://localhost:8080'
-        : 'https://obaview.azurewebsites.net',
+        : 'https://obaview.herokuapp.com',
     STOPS_FOR_ROUTE: '/stops-for-route/',
     STOP: '/stop/',
     ARRIVALS_DEPARTURES: '/arrivals-departures/',
